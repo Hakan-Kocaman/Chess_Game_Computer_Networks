51ec735fa97a5914b923a0577c1aba255babce20
Hakan Kocaman <dev.hkocaman@gmail.com>
2026-05-14 22:11:38 +0300

    close sockets end of the game

--- backend/services.py
+++ backend/services.py
@@ -206,6 +206,10 @@ def finish_game_service(winner,loser,result):
     )
     broadcast(response, reciever_list)
 
+    for player in player_list:
+        player.socket.close()
+    reset_server_service()
+
 
 def broadcast(response, reciever_list):
     logger.info(f"Broadcasting response for {response.URL} to {len(reciever_list)} clients.")
